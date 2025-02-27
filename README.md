5ee89d2675e5fd3808bb5296bfca591191f4a628
kjmin-dev <kjmin.kr@gmail.com>
2025-02-28 01:43:30 +0900

    CHORE: Run `Prettier`

--- README.md
+++ README.md
@@ -3,24 +3,31 @@
 # Getting Started
 
 ### Setup node.js
+
 ```sh
 nvm install # Install node.js version defined in .nvmrc
 ```
+
 ### Run prepare: execute prepare script for each repos
+
 ```sh
 yarn prepare
 ```
+
 ### Run dev: run all repos
+
 ```sh
 yarn dev
 ```
 
 ### Run build: build all repos
+
 ```sh
 yarn build
 ```
 
 ### Create new repo
+
 ```sh
 yarn new
 ```
--- services/ws_gateway/src/main.ts
+++ services/ws_gateway/src/main.ts
@@ -1,11 +1,11 @@
-import { Server } from "socket.io";
-import http from "node:http";
+import http from 'node:http';
+import { Server } from 'socket.io';
 
 const PORT = Number(process.env.PORT) || 4000;
 const httpServer = http.createServer();
 const io = new Server(httpServer);
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
     console.log(`New connection ${socket.id}`);
 });
 
