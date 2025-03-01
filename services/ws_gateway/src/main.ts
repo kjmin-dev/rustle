b333308569f3a4ce095031bd3a08f550f3749353
kjmin-dev <kjmin.kr@gmail.com>
2025-03-01 12:10:37 +0900

    Init `ws_gateway` logger

--- services/ws_gateway/src/main.ts
+++ services/ws_gateway/src/main.ts
@@ -1,14 +1,24 @@
 import http from 'node:http';
+import { pino } from 'pino';
 import { WebSocketServer } from 'ws';
 
 const PORT = Number(process.env.PORT) || 3100;
 const httpServer = http.createServer();
 const wss = new WebSocketServer({ server: httpServer });
+const logger = pino({
+    level: 'info',
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: process.env.NODE_ENV !== 'production',
+        },
+    },
+});
 
 wss.on('connection', (socket) => {
-    console.log(`New connection from ${socket}`);
+    logger.info(`New connection from ${socket}`);
 });
 
 httpServer.listen(PORT, () => {
-    console.log(`ws_gateway is running on port ${PORT}`);
+    logger.info(`ws_gateway is running on port ${PORT}`);
 });
