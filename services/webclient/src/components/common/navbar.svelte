97b5e155d14cff5fd23e643774ce5c252b30417d
kjmin-dev <kjmin.kr@gmail.com>
2025-03-02 16:58:32 +0900

    Define Navbar

---
+++ services/webclient/src/components/common/navbar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { DarkMode, Navbar, NavBrand, NavHamburger, NavLi, NavUl } from 'flowbite-svelte';
+</script>
+
+<Navbar fluid>
+    <NavBrand href="/">
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    </NavBrand>
+
+    <div class="flex md:order-2">
+        <DarkMode />
+        <NavHamburger />
+    </div>
+
+    <NavUl slideParams={{ duration: 500 }}>
+        <NavLi href="/">Home</NavLi>
+    </NavUl>
+</Navbar>
--- services/webclient/src/routes/demo/paraglide/+page.svelte
+++ services/webclient/src/routes/demo/paraglide/+page.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { page } from '$app/state';
-	import { i18n } from '$lib/i18n';
-	import * as m from '$lib/paraglide/messages.js';
-	import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
-	import { Alert, Button } from 'flowbite-svelte';
+    import { goto } from '$app/navigation';
+    import { page } from '$app/state';
+    import { i18n } from '$lib/i18n';
+    import * as m from '$lib/paraglide/messages.js';
+    import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
+    import { Alert, Button } from 'flowbite-svelte';
 
-	function switchToLanguage(newLanguage: AvailableLanguageTag) {
-		const canonicalPath = i18n.route(page.url.pathname);
-		const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
-		goto(localisedPath);
-	}
+    function switchToLanguage(newLanguage: AvailableLanguageTag) {
+        const canonicalPath = i18n.route(page.url.pathname);
+        const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
+        goto(localisedPath);
+    }
 </script>
 
 <Alert>
-		{m.hello_world({ name: 'SvelteKit User' })}
+    {m.hello_world({ name: 'SvelteKit User' })}
 </Alert>
 
 <div>
-	<Button onclick={() => switchToLanguage('en')}>en</Button>
-	<Button onclick={() => switchToLanguage('ko')}>ko</Button>
+    <Button onclick={() => switchToLanguage('en')}>en</Button>
+    <Button onclick={() => switchToLanguage('ko')}>ko</Button>
 </div>
