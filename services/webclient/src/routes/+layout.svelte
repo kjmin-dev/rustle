4c0d0d4552d6477b7f29814dab06e69cf6f38913
kjmin-dev <kjmin.kr@gmail.com>
2025-03-02 20:25:51 +0900

    Webclient: Enable dark mode

--- services/webclient/src/routes/+layout.svelte
+++ services/webclient/src/routes/+layout.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
+    import { browser } from '$app/environment';
     import { i18n } from '$lib/i18n';
     import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+    import { onMount } from 'svelte';
     import '../app.css';
     let { children } = $props();
+
+    onMount(() => {
+        if (browser === false) {
+            return;
+        }
+        const colorTheme = localStorage.getItem('color-theme');
+        const systemTheme = window.matchMedia?.('(prefers-color-scheme: dark)')?.matches;
+
+        if (colorTheme === 'dark' || (!colorTheme && systemTheme)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    });
 </script>
 
 <ParaglideJS {i18n}>
