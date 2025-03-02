68a4276759ee978157e3634203a3240e69051d08
kjmin-dev <kjmin.kr@gmail.com>
2025-03-02 15:33:11 +0900

    Webclient: init bundle analyzer and postcss

--- services/webclient/vite.config.ts
+++ services/webclient/vite.config.ts
@@ -1,37 +1,48 @@
-import { paraglide } from "@inlang/paraglide-sveltekit/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { svelteTesting } from "@testing-library/svelte/vite";
+import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { svelteTesting } from '@testing-library/svelte/vite';
 import { defineConfig } from 'vite';
+import bundleAnalyzer from 'vite-bundle-analyzer';
 
 export default defineConfig({
-    plugins: [sveltekit(), tailwindcss(), paraglide({
-        project: "./project.inlang",
-        outdir: "./src/lib/paraglide"
-    })],
+    plugins: [
+        sveltekit(),
+        tailwindcss(),
+        paraglide({
+            project: './project.inlang',
+            outdir: './src/lib/paraglide',
+        }),
+        bundleAnalyzer({
+            analyzerMode: 'static',
+        }),
+    ],
 
     test: {
-        workspace: [{
-            extends: "./vite.config.ts",
-            plugins: [svelteTesting()],
+        workspace: [
+            {
+                extends: './vite.config.ts',
+                plugins: [svelteTesting()],
 
-            test: {
-                name: "client",
-                environment: "jsdom",
-                clearMocks: true,
-                include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-                exclude: ['src/lib/server/**'],
-                setupFiles: ['./vitest-setup-client.ts']
-            }
-        }, {
-            extends: "./vite.config.ts",
+                test: {
+                    name: 'client',
+                    environment: 'jsdom',
+                    clearMocks: true,
+                    include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+                    exclude: ['src/lib/server/**'],
+                    setupFiles: ['./vitest-setup-client.ts'],
+                },
+            },
+            {
+                extends: './vite.config.ts',
 
-            test: {
-                name: "server",
-                environment: "node",
-                include: ['src/**/*.{test,spec}.{js,ts}'],
-                exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-            }
-        }]
-    }
+                test: {
+                    name: 'server',
+                    environment: 'node',
+                    include: ['src/**/*.{test,spec}.{js,ts}'],
+                    exclude: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+                },
+            },
+        ],
+    },
 });
